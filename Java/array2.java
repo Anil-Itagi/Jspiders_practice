ab3bbd8ee26a705560de84dffd6a6f619a9353db
Anil-Itagi <itagianil464@gmail.com>
2025-04-18 21:17:39 +0530

    Structur Psuedo elements

---
+++ Java/array2.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class array2 {
+    public static void main(String[] args) {
+        double a[] = new double[2];
+        a[0]=10.5;
+        a[1] = 20.9;
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+    }
+}
+
---
+++ Java/array3.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class array3 {
+    public static void main(String[] args) {
+        char a[] = new char[2];
+        a[0]='A';
+        a[1] = 'B';
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+    }
+}
