e96322e044fff0849b9426d856c4f9fa97ce29ae
Anil-Itagi <itagianil464@gmail.com>
2025-05-02 23:53:55 +0530

    create a java program replace all method

---
+++ Java/replaceAll/Replace.java
@@ -0,0 +1,7 @@
+public class Replace{
+    public static void main(String[] args) {
+        String str = "ME@41thodv%19%ERLOA&29Dinasge*";
+        System.out.println(str.replaceAll("[@#$%*0&-5aeiou]",""));
+    }
+
+}
