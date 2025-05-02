18864e22e053e8da1a9dcbbf57ab6e4a35167b4d
Anil-Itagi <itagianil464@gmail.com>
2025-05-03 00:08:27 +0530

    create a java program run time polymorphism

---
+++ Java/replaceAll/BikeClass.java
@@ -0,0 +1,42 @@
+class Bike {
+    void sound() {
+        System.out.println("all bikes make sound");
+    }
+}
+
+class Java extends Bike {
+    void sound() {
+        System.out.println("bur bur java");
+    }
+}
+
+class Pulser extends Bike {
+    void sound() {
+        System.out.println("Pulser dur dur dur");
+    }
+}
+
+class BMW extends Bike {
+    void sound() {
+        System.out.println("DMW make sound uy uy uy");
+    }
+}
+
+class Simulator {
+    public static void veh_sound(Bike bike) {
+        bike.sound();
+    }
+}
+
+
+public class BikeClass {
+    public static void main(String[] args) {
+        Java j1 = new Java();
+        Pulser p1 = new Pulser();
+        BMW b1 = new BMW();
+
+        Simulator.veh_sound(b1);
+        Simulator.veh_sound(p1);
+        Simulator.veh_sound(j1);
+    }
+}
